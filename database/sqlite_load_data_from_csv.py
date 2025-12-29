8f5def21a4d1484c787d62c4614f74ac9aa68cb5
Konstantin <ray999k@icloud.com>
2025-12-29 22:18:50 +0300

    Update sqlite_load_data_from_csv.py

--- database/sqlite_load_data_from_csv.py
+++ database/sqlite_load_data_from_csv.py
@@ -4,25 +4,19 @@ import glob
 import os
 
 
-class loadDataToSQLite:
-
-    def __init__(self, path_input_files, path_database):
-        self.path_input_files = path_input_files
-        self.path_database = path_database
-
-    def load_data(self):
-        os.chdir(self.path_input_files)
-        conn = sqlite3.connect(self.path_database)
-        cursor = conn.cursor()
-        csv_files = glob.glob('*.csv')
-        for file in csv_files:
-            with open(file, 'r', encoding='utf-8') as f:
-                reader = csv.reader(f)
-                header = next(reader)
-                print(file)
-                create_table = f"CREATE TABLE IF NOT EXISTS {file.replace('.csv', '')} ({', '.join(header)})"
-                cursor.execute(create_table)
-                insert_query = f"INSERT INTO table_name VALUES ({', '.join(['?'] * len(header))})"
-                cursor.executemany(insert_query, reader)
-        conn.commit()
-        conn.close()
+def sqlite_load_data_from_csv(path_input_files, path_database):
+    os.chdir(path_input_files)
+    conn = sqlite3.connect(path_database)
+    cursor = conn.cursor()
+    csv_files = glob.glob('*.csv')
+    for file in csv_files:
+        with open(file, 'r', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            header = next(reader)
+            print(file)
+            create_table = f"CREATE TABLE IF NOT EXISTS {file.replace('.csv', '')} ({', '.join(header)})"
+            cursor.execute(create_table)
+            insert_query = f"INSERT INTO table_name VALUES ({', '.join(['?'] * len(header))})"
+            cursor.executemany(insert_query, reader)
+    conn.commit()
+    conn.close()
